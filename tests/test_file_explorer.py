1ec4e5857c3144e122f43eddbae1bedfc868b5bb
Vadym Moshynskyi <vmoshynskyi@indocresearch.org>
2025-09-01 17:32:19 +0200

    IEBH-449: Refactor file explorer tests (#1)

--- tests/test_file_explorer.py
+++ tests/test_file_explorer.py
@@ -5,8 +5,12 @@
 # You may not use this file except in compliance with the License.
 
 import hashlib
+import io
 import os
 import re
+import zipfile
+from collections.abc import Generator
+from contextlib import contextmanager
 from pathlib import Path
 from typing import Self
 
@@ -14,6 +18,7 @@ from playwright.sync_api import Download
 from playwright.sync_api import FilePayload
 from playwright.sync_api import Locator
 from playwright.sync_api import Page
+from playwright.sync_api import Response
 from playwright.sync_api import TimeoutError as PlaywrightTimeoutError
 from playwright.sync_api import expect
 from pydantic import BaseModel
@@ -35,6 +40,12 @@ class File(BaseModel):
 
         return cls(name=name, content=content, hash=file_hash, tags=file_tags)
 
+    def save_to_folder(self, folder: Path) -> Path:
+        file_path = folder / self.name
+        with open(file_path, 'wb') as f:
+            f.write(self.content)
+        return file_path
+
 
 class FileExplorer:
     def __init__(self, page: Page) -> None:
@@ -52,12 +63,26 @@ class FileExplorer:
 
         return download_info.value
 
-    def download_and_get_hash(self, names: list[str]) -> str:
+    def download_and_get_content(self, names: list[str]) -> bytes:
         download = self.download(names)
         with open(download.path(), 'rb') as f:
-            received_file_content = f.read()
+            return f.read()
 
-        return hashlib.sha1(received_file_content).hexdigest()
+    def download_and_get_hash(self, names: list[str]) -> str:
+        file_content = self.download_and_get_content(names)
+        return hashlib.sha1(file_content).hexdigest()
+
+    def download_and_extract_files(self, names: list[str]) -> Generator[File]:
+        file_content = self.download_and_get_content(names)
+
+        with zipfile.ZipFile(io.BytesIO(file_content)) as zf:
+            for fileinfo in zf.infolist():
+                if fileinfo.is_dir():
+                    continue
+                with zf.open(fileinfo.filename) as file:
+                    file_content = file.read()
+                    file_hash = hashlib.sha1(file_content).hexdigest()
+                    yield File(name=fileinfo.filename, content=file_content, hash=file_hash)
 
     def navigate_to(self, folder_path: Path, *, create_missing_folders: bool = False) -> None:
         for folder in folder_path.parts:
@@ -107,8 +132,40 @@ class FileExplorer:
         for tag in file.tags:
             tags_input.fill(tag)
             tags_input.press('Enter')
+            tags_input.press('Escape')
 
         self.page.get_by_role('button', name='cloud-upload Upload', exact=True).click()
+
+    def upload_folder(self, folder_path: Path) -> None:
+        self.page.get_by_role('button', name='upload Upload', exact=True).click()
+
+        dialog = self.page.get_by_role('dialog')
+
+        folder_input = dialog.locator('#form_in_modal_folder')
+        folder_input.set_input_files(folder_path)
+
+        self.page.get_by_role('button', name='cloud-upload Upload', exact=True).click()
+
+    @contextmanager
+    def wait_until_uploaded(self, names: list[str], wait_for_refresh: bool = True) -> Generator[None]:
+        files_to_upload = set(names)
+
+        def check_response(response: Response) -> bool:
+            if response.url.endswith('pilot/upload/gr/v1/files') and response.request.method == 'POST':
+                filename = response.request.post_data_json['resumable_filename']
+                if filename in files_to_upload:
+                    files_to_upload.remove(filename)
+
+            if files_to_upload:
+                return False
+
+            if not wait_for_refresh:
+                return True
+
+            return 'v1/files/meta?' in response.url
+
+        with self.page.expect_response(check_response):
+            yield
 
 
 def test_file_upload_and_download(admin_page: Page, project_code: str, working_path: Path) -> None:
@@ -119,7 +176,8 @@ def test_file_upload_and_download(admin_page: Page, project_code: str, working_p
     file_explorer.navigate_to(working_path / 'file-upload', create_missing_folders=True)
 
     file = File.generate()
-    file_explorer.upload_file(file)
+    with file_explorer.wait_until_uploaded([file.name]):
+        file_explorer.upload_file(file)
 
     received_file_hash = file_explorer.download_and_get_hash([file.name])
 
@@ -134,7 +192,8 @@ def test_file_upload_with_tags(admin_page: Page, project_code: str, working_path
     file_explorer.navigate_to(working_path / 'file-upload', create_missing_folders=True)
 
     file = File.generate(tags_number=3)
-    file_explorer.upload_file(file)
+    with file_explorer.wait_until_uploaded([file.name]):
+        file_explorer.upload_file(file)
 
     file_explorer.locate_file(file.name).get_by_label('more').hover()
     admin_page.get_by_role('menuitem', name='Properties').click()
@@ -145,6 +204,31 @@ def test_file_upload_with_tags(admin_page: Page, project_code: str, working_path
     assert set(file.tags) == set(received_tags)
 
 
+def test_folder_upload_and_download(admin_page: Page, project_code: str, working_path: Path, tmp_path: Path) -> None:
+    """Test that a folder can be uploaded and then downloaded successfully."""
+
+    file_explorer = FileExplorer(admin_page)
+    file_explorer.open_project(project_code)
+    file_explorer.navigate_to(working_path / 'folder-upload', create_missing_folders=True)
+
+    folder_name = f'e2e-test-{os.urandom(5).hex()}'
+    folder_path = tmp_path / folder_name
+    folder_path.mkdir()
+
+    file_1 = File.generate()
+    file_1.save_to_folder(folder_path)
+    file_2 = File.generate()
+    file_2.save_to_folder(folder_path)
+
+    with file_explorer.wait_until_uploaded([file_1.name, file_2.name]):
+        file_explorer.upload_folder(folder_path)
+
+    received_files = list(file_explorer.download_and_extract_files([folder_name]))
+
+    assert len(received_files) == 2
+    assert {file_1.hash, file_2.hash} == {f.hash for f in received_files}
+
+
 def test_file_resumable_upload_and_download(admin_page: Page, project_code: str, working_path: Path) -> None:
     """Test that an interrupted file upload can be resumed and then successfully downloaded."""
 
@@ -152,23 +236,21 @@ def test_file_resumable_upload_and_download(admin_page: Page, project_code: str,
     file_explorer.open_project(project_code)
     file_explorer.navigate_to(working_path / 'file-resume-upload', create_missing_folders=True)
 
-    admin_page.locator('a.ant-notification-notice-close').click()
-
     file = File.generate(size_mb=10)
-    file_explorer.upload_file(file)
+    with admin_page.expect_response(
+        lambda r: r.url.endswith(f'project/{project_code}/files') and r.request.method == 'POST'
+    ):
+        file_explorer.upload_file(file)
 
-    first_file_status_line = admin_page.get_by_role('heading').first
-
-    expect(first_file_status_line).to_have_text(re.compile(r'Waiting'))
     admin_page.reload()
 
     admin_page.locator('span.ant-badge-status').click()
-    expect(first_file_status_line).to_have_text(re.compile(r'Re-upload file'))
+    first_file_status_line = admin_page.get_by_role('heading', name=re.compile(r'Re-upload file')).first
 
-    first_file_status_line.locator('input[type="file"]').set_input_files(
-        FilePayload(name=file.name, mimeType='application/octet-stream', buffer=file.content)
-    )
-    expect(first_file_status_line).to_have_text(re.compile(r'Uploaded'))
+    with file_explorer.wait_until_uploaded([file.name], wait_for_refresh=False):
+        first_file_status_line.locator('input[type="file"]').set_input_files(
+            FilePayload(name=file.name, mimeType='application/octet-stream', buffer=file.content)
+        )
 
     file_explorer.navigate_to(working_path / 'file-resume-upload')
 
