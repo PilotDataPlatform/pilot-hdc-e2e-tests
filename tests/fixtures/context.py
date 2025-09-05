11f1cbd0477390f5b934858c461b9b33cbc58961
Vadym Moshynskyi <vmoshynskyi@indocresearch.org>
2025-09-05 18:04:16 +0200

    IEBH-449: Make screenshots with failures

--- tests/fixtures/context.py
+++ tests/fixtures/context.py
@@ -8,39 +8,31 @@ import datetime as dt
 import os
 from collections.abc import Generator
 from pathlib import Path
-from typing import Any
 
 import pytest
-from playwright.sync_api import Browser
 from playwright.sync_api import BrowserContext
 from playwright.sync_api import Page
 from playwright.sync_api import ViewportSize
 from playwright.sync_api import expect
 from pytest import TempPathFactory
+from pytest_playwright import CreateContextCallback
 
 
 class Contexts:
-    def __init__(self, browser: Browser, browser_context_args: dict[str, Any], storage_state_dir: Path) -> None:
-        self.browser = browser
-        self.browser_context_args = browser_context_args
+    def __init__(self, storage_state_dir: Path) -> None:
         self.storage_state_dir = storage_state_dir
         self.viewport = ViewportSize(width=1280, height=1024)
 
         self.users: dict[str, str] = {}
-        self.contexts: dict[str, BrowserContext] = {}
+        self.states: dict[str, Path] = {}
 
     def add_user(self, username: str, password: str) -> None:
         self.users[username] = password
 
-    def close(self) -> None:
-        for context in self.contexts.values():
-            context.close()
-        self.contexts.clear()
-
-    def login(self, username: str) -> Path:
+    def login(self, new_context: CreateContextCallback, username: str) -> Path:
         password = self.users[username]
 
-        context = self.browser.new_context(**self.browser_context_args)
+        context = new_context(viewport=self.viewport)
         page = context.new_page()
 
         page.goto('/login')
@@ -59,24 +51,14 @@ class Contexts:
 
         storage_state = self.storage_state_dir / f'{username}.json'
         context.storage_state(path=storage_state)
-        context.close()
+        page.close()
 
         return storage_state
 
-    def get_context_for(self, username: str) -> BrowserContext:
-        if username not in self.contexts:
-            storage_state = self.login(username)
-            self.contexts[username] = self.browser.new_context(
-                **self.browser_context_args,
-                storage_state=storage_state,
-                viewport=self.viewport,
-            )
-
-        return self.contexts[username]
-
-    def get_page_for(self, username: str) -> Page:
-        context = self.get_context_for(username)
-        return context.new_page()
+    def get_context_for(self, new_context: CreateContextCallback, username: str) -> BrowserContext:
+        if username not in self.states:
+            self.states[username] = self.login(new_context, username)
+        return new_context(storage_state=self.states[username], viewport=self.viewport)
 
 
 @pytest.fixture(scope='session')
@@ -85,21 +67,11 @@ def storage_state_dir(tmp_path_factory: TempPathFactory) -> Path:
 
 
 @pytest.fixture(scope='session')
-def contexts(
-    admin_username: str,
-    collaborator_username: str,
-    browser: Browser,
-    browser_context_args: dict[str, Any],
-    storage_state_dir: Path,
-) -> Generator[Contexts]:
-    contexts = Contexts(browser, browser_context_args, storage_state_dir)
-
+def contexts(admin_username: str, collaborator_username: str, storage_state_dir: Path) -> Contexts:
+    contexts = Contexts(storage_state_dir)
     contexts.add_user(admin_username, os.environ.get('E2E_TESTING_ADMIN_PASSWORD', ''))
     contexts.add_user(collaborator_username, os.environ.get('E2E_TESTING_COLLABORATOR_PASSWORD', ''))
-
-    yield contexts
-
-    contexts.close()
+    return contexts
 
 
 @pytest.fixture(scope='session')
@@ -118,15 +90,17 @@ def collaborator_username() -> str:
 
 
 @pytest.fixture
-def admin_page(contexts: Contexts, admin_username: str) -> Generator[Page]:
-    with contexts.get_page_for(admin_username) as page:
-        yield page
+def admin_page(contexts: Contexts, new_context: CreateContextCallback, admin_username: str) -> Generator[Page]:
+    with contexts.get_context_for(new_context, admin_username) as context:
+        yield context.new_page()
 
 
 @pytest.fixture
-def collaborator_page(contexts: Contexts, collaborator_username: str) -> Generator[Page]:
-    with contexts.get_page_for(collaborator_username) as page:
-        yield page
+def collaborator_page(
+    contexts: Contexts, new_context: CreateContextCallback, collaborator_username: str
+) -> Generator[Page]:
+    with contexts.get_context_for(new_context, collaborator_username) as context:
+        yield context.new_page()
 
 
 @pytest.fixture()
